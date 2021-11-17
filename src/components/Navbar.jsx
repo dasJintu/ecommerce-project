0dbd8a3ad9cadab5559ae293f20374401c7b75e9
Jintu Das <jintudas047@gmail.com>
2021-11-17 08:53:51 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const navigation = [
   "men's clothing",
@@ -10,6 +11,7 @@ const navigation = [
 
 export default function Navbar() {
   const [mobileMenu, setMobileMenu] = useState(false);
+  const cartList = useSelector((state) => state.cart.cartList);
 
   return (
     <nav className="bg-white w-full shadow-lg">
@@ -51,18 +53,23 @@ export default function Navbar() {
         </div>
 
         <Link to="/cart">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-7 w-7 -rotT text-green-600"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <div className="relative">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-8 w-8 text-gray-700"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <p className="absolute top-0 right-0 bg-red-400 rounded-full h-4 w-4 flex justify-center items-center">
+              <span className="text-sm font-bold">{cartList.length}</span>
+            </p>
+          </div>
         </Link>
       </div>
 
